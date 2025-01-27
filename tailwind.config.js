fe50d1536978e401b37e3ac254aa9e666f603193
Benjamin Jurewicz <94708038+BenJurewicz@users.noreply.github.com>
2025-01-27 22:41:11 +0100

    Update to accommodate tailwind v4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: "all",
-  content: ["./src/**/*.{rs,html,css}", "./dist/**/*.html"],
+  content: [
+    // Include all Rust, HTML, and CSS files in the src directory
+    './src/**/*.{rs,html,css}',
+    // Include all HTML files in the output (dist) directory
+    './dist/**/*.html',
+  ],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
-};
+}
