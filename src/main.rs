0cfedb82f59ec5b6219cec1a0e7984151ace3847
Benjamin Jurewicz <94708038+BenJurewicz@users.noreply.github.com>
2025-02-24 04:59:30 +0100

    Removed some notes

--- src/main.rs
+++ src/main.rs
@@ -12,33 +12,6 @@ fn main() {
     dioxus::launch(App);
 }
 
-/*
-Source:
-    https://dioxuslabs.com/learn/0.5/cookbook/state/custom_hooks#custom-hook-logic
-General idea for handling the sudoku state
-    Create a custom hook that will return a signal wrapper with these methods
-    - get_cell( point: Point<u8, u8> ) -> u8 (it has to be more than u8, some kind of use_memo since it's just a view)
-        returns the value of the given cell coordinate; useful for rendering the cell view (currently named tile)
-    - get_whole() -> [[u8; 9] 9]
-        returns the whole sudoku array; will be useful when implementing the button to solve the sudoku
-    - get_update_func( point: Point<u8, u8> ) -> Fn<takes in u8 (don't remember the syntax)>
-        takes in the coordinate of the cell
-        returns a method for updating the cell with the given coordinate
-        the method takes in the value that the cell should be changed to and returns nothing
-        useful for implementing the oninput for the cell view (currently named tile)
-    Note: maybe use x: u8, y:u8 instead of point?
-
-    how to use in code:
-        fn App() -> Element {
-            let mut sudoku = use_sudoku();
-
-            rsx! {
-                Tile {
-                    value: sudoku.get_cell(Point::new(0,0)),
-                    oninput: sudoku.get_update_func(Point::new(0,0))
-                }
-        }
-*/
 #[component]
 fn App() -> Element {
     let mut board = use_signal(|| [[Some::<u8>(2); 9]; 9]);
