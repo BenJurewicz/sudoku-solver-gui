9d580865543d7150323d2a4cdbc9d7b0737e3876
Benjamin Jurewicz <94708038+BenJurewicz@users.noreply.github.com>
2025-03-12 16:42:11 +0100

    highlight same numbers on the board

--- src/numberInput.rs
+++ src/numberInput.rs
@@ -12,12 +12,16 @@ pub fn NumberInput(board: Signal<Sudoku>, focused: Signal<Option<(usize, usize)>
             class: "grid grid-cols-3 gap-[5%] mx-10 size-40 md:size-50 max-w justify-center items-center",
             for i in 1..=9 {
                 button {
-                    disabled: focused.read().is_none(),
+                    disabled: focused.read().map_or(false, |(fx, fy)| board.read().is_read_only(fx, fy)),
                     class: "transition text-3xl md:text-4xl aspect-square bg-gray-500 text-white font-bold border-solid border-gray-800 border-2 rounded-lg \
                             enabled:hover:bg-gray-600 enabled:active:bg-gray-700 \
                             disabled:opacity-40",
-                    onclick: move |_| {
-                        let selected = focused.read().clone();
+                    onclick: move |e| {
+                        e.stop_propagation();
+                        let selected = match focused.read().clone() {
+                            Some((fx, fy)) if !board.read().is_read_only(fx, fy) => Some((fx, fy)),
+                            _ => None
+                        };
                         if let Some((fx, fy)) = selected {
                             board.write().set_cell(fx, fy, Some(NonZeroU8::new(i).unwrap()));
                             if(board.read().check()){
--- src/tile.rs
+++ src/tile.rs
@@ -7,6 +7,9 @@ use crate::sudoku::Sudoku;
 #[component]
 pub fn Tile(board: Signal<Sudoku>, focused:Signal<Option<(usize, usize)>>, message: Signal<Message>, x: usize, y: usize) -> Element {
     let handleInput = move |e : KeyboardEvent| {
+        if board.read().is_read_only(x, y) {
+            return;
+        }
         if let Key::Character(c) = e.key() {
             let num =
                 c.parse::<u8>().ok()
@@ -29,14 +32,16 @@ pub fn Tile(board: Signal<Sudoku>, focused:Signal<Option<(usize, usize)>>, messa
         }
     };
 
-    // TODO this sohuld be use memo or whatever its called
-    let shown_value = if let Some(v) = board.read().get_cell(x, y) {
-        v.to_string()
-    } else {
-        String::from("")
-    };
+    let shown_value = use_memo(move || {
+        if let Some(v) = board.read().get_cell(x, y) {
+            v.to_string()
+        } else {
+            String::from("")
+        }});
 
-    let focus_neighbour = match focused() {
+    let is_focused = focused.read().map_or(false, |(fx, fy)| x == fx && y == fy);
+
+    let is_focused_neighbour = match focused() {
         Some((fx, fy)) => {
             // Checks whether the focused cell has direct influence on
             // the value of the current cell
@@ -44,7 +49,13 @@ pub fn Tile(board: Signal<Sudoku>, focused:Signal<Option<(usize, usize)>>, messa
         },
         _ => false
     };
+
     let is_read_only = board.read().is_read_only(x, y);
+
+    let same_as_focused = match focused() {
+        Some((fx, fy)) => board.read().get_cell(x, y) == board.read().get_cell(fx, fy),
+        _ => false
+    };
 
     rsx! {
         div {
@@ -52,15 +63,25 @@ pub fn Tile(board: Signal<Sudoku>, focused:Signal<Option<(usize, usize)>>, messa
             // We want the board to take up around 2/3 of the screen and sudoku grid is 9x9 so:
             // x * 9 / 100 = 2/3
             // x = 7,4 ≈ 2/3 * 100/9
-            class: format!("size-[9vmin] sm:size-[7.4vmin] text-[6vmin] md:text-[4.5vmin] lg:text-[3vmin] flex items-center justify-center hover:bg-gray-300 active:bg-gray-500 focus:bg-gray-400 focus:font-semibold  {}",
-                if is_read_only {"!bg-zinc-600 !text-white !font-bold"} else if focus_neighbour {"bg-gray-200"} else {""}),
+            class: format!("size-[9vmin] sm:size-[7.4vmin] text-[6vmin] md:text-[4.5vmin] lg:text-[3vmin] text-center select-none cursor-pointer {} {}",
+                if is_read_only && !is_focused {"bg-zinc-600 text-white font-bold"}
+                else if is_read_only && is_focused {"bg-zinc-700 text-white font-bold"}
+                else if is_focused {"bg-gray-400 font-semibold"}
+                else if is_focused_neighbour {"bg-gray-200"}
+                else {"hover:bg-gray-300 active:bg-gray-500"},
+
+                if is_focused {""}
+                else if same_as_focused && is_focused_neighbour {"!text-red-400"}
+                else if same_as_focused && !is_focused_neighbour {"!text-green-400"}
+                else {""}),
+
             onkeydown: handleInput,
             onclick: move |e| {
                 // Prevents the item from being unfocused by onclick handler in root div
                 e.stop_propagation();
-                if(!is_read_only) {
+                // if(!is_read_only) {
                     focused.set(Some((x, y)));
-                }
+                // }
             },
             "{shown_value}"
         }
